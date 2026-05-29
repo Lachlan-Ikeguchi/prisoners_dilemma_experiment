3bce078b210c895c0eac6e77a0890076b54c4762
Lachlan Ikeguchi <lachlan_ikeguchi@proton.me>
2026-05-30 06:58:29 +1000

    fix: move randomseed inside decide function in CRAZED.lua

--- contestents/CRAZED.lua
+++ contestents/CRAZED.lua
@@ -1,9 +1,8 @@
 -- name: CRAZED
 -- completely unpredictable
 
-math.randomseed(os.time() * round)
-
 function decide(round, history)
+    math.randomseed(os.time() * round)
     if math.random() < 0.5001 then
         return "cooperate"
     else
